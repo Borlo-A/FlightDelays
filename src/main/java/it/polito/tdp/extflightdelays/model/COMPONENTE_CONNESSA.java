834d2f245f78f66f1abb4e066e31d9b049590468
s272034 <s272034@studenti.poltio.it>
2022-06-29 09:43:26 +0200

    Ultimissima

---
+++ src/main/java/it/polito/tdp/extflightdelays/model/COMPONENTE_CONNESSA.java
@@ -0,0 +1,20 @@
+package it.polito.tdp.extflightdelays.model;
+
+public class COMPONENTE_CONNESSA {
+	//public Set<Country> getComponenteConnessa(Country vertice){
+	//Set<Country> visitati= new HashSet<>(); 
+	//DepthFirstIterator <Country, DefaultEdge> it= new DepthFirstIterator  <Country, DefaultEdge>(this.grafo, vertice);
+	//while(it.hasNext()) {
+//		visitati.add(it.next()); 
+	//}
+	//return visitati; 
+	//}
+	//
+	//public Set<Country> getComponenteConnessa2(Country vertice){
+	//ConnectivityInspector<Country, DefaultEdge> ci = new ConnectivityInspector<Country, DefaultEdge>(this.grafo);
+	//Set<Country> visitati= new HashSet<>(); 
+	//visitati= ci.connectedSetOf(vertice);
+	//return visitati; 
+	//}
+
+}
